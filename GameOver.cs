3d023fd707d20fab2d44adb136e389a844e33aeb
karan-monga <38899841+karan-monga@users.noreply.github.com>
2019-03-26 14:44:23 -0700

    Add files via upload

---
+++ GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.name == "Player")
+            Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
